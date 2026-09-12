cfbbdf0e8c2246b175b860dcf1beb22034e6c2ca
言午 <2291767568@qq.com>
2026-09-12 10:40:28 +0800

    数据看板：fetch加载数据与统计卡片

--- my-dashboard/app.js
+++ my-dashboard/app.js
@@ -25,4 +25,34 @@ const loadData = async () => {
     }
 }
 
+const renderCards = (data) => {
+    const total_used = {
+        "楠苑": [0, 0], 
+        "梓苑": [0, 0], 
+        "图书馆": [0, 0], 
+        "理科楼": [0, 0], 
+        "文科楼": [0, 0],
+    };
+
+
+    data.rooms.forEach(room => {
+        total_used[room.building][0] += room.occupied;
+        total_used[room.building][1] += room.seats;
+    });
+
+    Object.entries(total_used).forEach(([key, value]) => {
+        $("#cards").append(`
+        <div class="col-md-4">
+            <div class="card">
+            <div class="card-body">
+                <h3 class="card-title h6">${key}</h3>
+                <p class="card-text fs-4">${value[0]} / ${value[1]}</p>
+                <p class="card-text small text-muted">已使用 / 座位总数</p>
+            </div>
+            </div>
+        </div>
+        `);
+    });
+};
+
 loadData();
